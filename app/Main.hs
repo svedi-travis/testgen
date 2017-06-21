631f24cb20fcae329df0651724ddf17e1f7f786e
Martin Svedin <svedi@kth.se>
2017-06-21 17:36:28 +0000

    THIS IS NOT A COMMIT MESSAGE

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,294 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import           Control.Lens
+import           Control.Monad
+import qualified Data.Aeson             as Aeson
+import qualified Data.Aeson.Lens        as Aeson
+import qualified Data.ByteString        as B
+import qualified Data.ByteString.Lazy   as BL
+import qualified Data.ByteString.Base64 as B
+import           Data.List              as List
+import qualified Data.Text              as Text
+import qualified Data.Text.Encoding     as Text
+import qualified Network.Wreq           as Wreq
+import           System.Random
+
+import qualified Lib as SKM
+
+k1 :: SKM.Key
+k1 = SKM.KeyContents
+  { SKM.productId = 3720
+  , SKM.id = 1
+  , SKM.key = "BIGYF-VFZPK-CWZLH-RXVRL"
+  , SKM.created = 0
+  , SKM.expires = 0
+  , SKM.period = 30
+  , SKM.f1 = False
+  , SKM.f2 = True
+  , SKM.f3 = False
+  , SKM.f4 = True
+  , SKM.f5 = False
+  , SKM.f6 = True
+  , SKM.f7 = False
+  , SKM.f8 = True
+  , SKM.notes = ""
+  , SKM.block = False
+  , SKM.globalId = 0
+  , SKM.customer = ()
+  , SKM.activatedMachines = []
+  , SKM.trialActivation = False
+  , SKM.maxnoofmachines = 0
+  , SKM.allowedMachines = []
+  , SKM.dataObjects = []
+  , SKM.signDate = 0
+  }
+
+k2 :: SKM.Key
+k2 = SKM.KeyContents
+  { SKM.productId = 3720
+  , SKM.id = 2
+  , SKM.key = "FDUXP-HSYRL-HFYWV-DGBZU"
+  , SKM.created = 0
+  , SKM.expires = 0
+  , SKM.period = 40
+  , SKM.f1 = True
+  , SKM.f2 = False
+  , SKM.f3 = True
+  , SKM.f4 = False
+  , SKM.f5 = True
+  , SKM.f6 = False
+  , SKM.f7 = True
+  , SKM.f8 = False
+  , SKM.notes = "TestKey1"
+  , SKM.block = False
+  , SKM.globalId = 0
+  , SKM.customer = ()
+  , SKM.activatedMachines = []
+  , SKM.trialActivation = False
+  , SKM.maxnoofmachines = 0
+  , SKM.allowedMachines = []
+  , SKM.dataObjects = []
+  , SKM.signDate = 0
+  }
+
+k3 :: SKM.Key
+k3 = SKM.KeyContents
+  { SKM.productId = 3720
+  , SKM.id = 3
+  , SKM.key = "HHLMY-JJIHL-CCJOZ-FQOPV"
+  , SKM.created = 0
+  , SKM.expires = 0
+  , SKM.period = 30
+  , SKM.f1 = True
+  , SKM.f2 = True
+  , SKM.f3 = True
+  , SKM.f4 = True
+  , SKM.f5 = False
+  , SKM.f6 = False
+  , SKM.f7 = False
+  , SKM.f8 = False
+  , SKM.notes = "Second Test Key"
+  , SKM.block = False
+  , SKM.globalId = 0
+  , SKM.customer = ()
+  , SKM.activatedMachines = []
+  , SKM.trialActivation = False
+  , SKM.maxnoofmachines = 0
+  , SKM.allowedMachines = []
+  , SKM.dataObjects = []
+  , SKM.signDate = 0
+  }
+
+k4 :: SKM.Key
+k4 = SKM.KeyContents
+  { SKM.productId = 3720
+  , SKM.id = 4
+  , SKM.key = "BLKWA-CMBGP-VPBKO-IQGEE"
+  , SKM.created = 0
+  , SKM.expires = 0
+  , SKM.period = 30
+  , SKM.f1 = False
+  , SKM.f2 = False
+  , SKM.f3 = False
+  , SKM.f4 = False
+  , SKM.f5 = True
+  , SKM.f6 = True
+  , SKM.f7 = True
+  , SKM.f8 = True
+  , SKM.notes = "One Data Object"
+  , SKM.block = False
+  , SKM.globalId = 0
+  , SKM.customer = ()
+  , SKM.activatedMachines = []
+  , SKM.trialActivation = False
+  , SKM.maxnoofmachines = 0
+  , SKM.allowedMachines = []
+  , SKM.dataObjects = []
+  , SKM.signDate = 0
+  }
+
+k5 :: SKM.Key
+k5 = SKM.KeyContents
+  { SKM.productId = 3720
+  , SKM.id = 5
+  , SKM.key = "BNDAZ-PBIMV-AJRTZ-LXYJT"
+  , SKM.created = 0
+  , SKM.expires = 0
+  , SKM.period = 30
+  , SKM.f1 = True
+  , SKM.f2 = False
+  , SKM.f3 = True
+  , SKM.f4 = False
+  , SKM.f5 = False
+  , SKM.f6 = True
+  , SKM.f7 = False
+  , SKM.f8 = True
+  , SKM.notes = "Two Data Objects"
+  , SKM.block = False
+  , SKM.globalId = 0
+  , SKM.customer = ()
+  , SKM.activatedMachines = []
+  , SKM.trialActivation = False
+  , SKM.maxnoofmachines = 0
+  , SKM.allowedMachines = []
+  , SKM.dataObjects = []
+  , SKM.signDate = 0
+  }
+
+header :: String
+header = concat
+ [ "#include <iostream>\n"
+ , "\n"
+ , "#include \"gtest/gtest.h\"\n"
+ , "\n"
+ , "#include \"basic_SKM.hpp\"\n"
+ , "#include \"LicenseKey.hpp\"\n"
+ , "#include \"LicenseKeyChecker.hpp\"\n"
+ , "#include \"RawLicenseKey.hpp\"\n"
+ , "\n"
+ , "#include \"RequestHandler_static.hpp\"\n"
+ , "#include \"SignatureVerifier_OpenSSL.hpp\"\n"
+ , "\n"
+ , "using namespace serialkeymanager_com;\n"
+ , "\n"
+ , "struct FeatureChecker\n"
+ , "{\n"
+ , "  std::string license;\n"
+ , "  bool f1;\n"
+ , "  bool f2;\n"
+ , "  bool f3;\n"
+ , "  bool f4;\n"
+ , "  bool f5;\n"
+ , "  bool f6;\n"
+ , "  bool f7;\n"
+ , "  bool f8;\n"
+ , "\n"
+ , "  void check() const\n"
+ , "  {\n"
+ , "    optional<LicenseKey> license_key = LicenseKey::make_unsafe(license);\n"
+ , "\n"
+ , "    ASSERT_TRUE(license_key.has_value()) << \"Failed to construct LicenseKey object\";\n"
+ , "\n"
+ , "    EXPECT_EQ(license_key->get_f1(), f1);\n"
+ , "    EXPECT_EQ(license_key->get_f2(), f2);\n"
+ , "    EXPECT_EQ(license_key->get_f3(), f3);\n"
+ , "    EXPECT_EQ(license_key->get_f4(), f4);\n"
+ , "    EXPECT_EQ(license_key->get_f5(), f5);\n"
+ , "    EXPECT_EQ(license_key->get_f6(), f6);\n"
+ , "    EXPECT_EQ(license_key->get_f7(), f7);\n"
+ , "    EXPECT_EQ(license_key->get_f8(), f8);\n"
+ , "\n"
+ , "    EXPECT_EQ((bool)license_key->check().has_feature(1), f1);\n"
+ , "    EXPECT_EQ((bool)license_key->check().has_feature(2), f2);\n"
+ , "    EXPECT_EQ((bool)license_key->check().has_feature(3), f3);\n"
+ , "    EXPECT_EQ((bool)license_key->check().has_feature(4), f4);\n"
+ , "    EXPECT_EQ((bool)license_key->check().has_feature(5), f5);\n"
+ , "    EXPECT_EQ((bool)license_key->check().has_feature(6), f6);\n"
+ , "    EXPECT_EQ((bool)license_key->check().has_feature(7), f7);\n"
+ , "    EXPECT_EQ((bool)license_key->check().has_feature(8), f8);\n"
+ , "\n"
+ , "    EXPECT_EQ((bool)license_key->check().has_not_feature(1), !f1);\n"
+ , "    EXPECT_EQ((bool)license_key->check().has_not_feature(2), !f2);\n"
+ , "    EXPECT_EQ((bool)license_key->check().has_not_feature(3), !f3);\n"
+ , "    EXPECT_EQ((bool)license_key->check().has_not_feature(4), !f4);\n"
+ , "    EXPECT_EQ((bool)license_key->check().has_not_feature(5), !f5);\n"
+ , "    EXPECT_EQ((bool)license_key->check().has_not_feature(6), !f6);\n"
+ , "    EXPECT_EQ((bool)license_key->check().has_not_feature(7), !f7);\n"
+ , "    EXPECT_EQ((bool)license_key->check().has_not_feature(8), !f8);\n"
+ , "  }\n"
+ , "};\n"
+ , "\n"
+ , "int\n"
+ , "main(int argc, char **argv)\n"
+ , "{\n"
+ , "  ::testing::InitGoogleTest(&argc, argv);\n"
+ , "  return RUN_ALL_TESTS();\n"
+ , "}\n"
+ , "\n"
+ ]
 
 main :: IO ()
-main = putStrLn "asdfasdf"
+main = do
+  let keys = [ ("Key1", k1), ("Key2", k2), ("Key3", k3), ("Key4", k4), ("Key5", k5)]
+  putStrLn header
+  forM_ keys (\(name, key) -> generateTestCaseForKey name key >>= putStrLn)
+
+generateTestCaseForKey :: String -> SKM.Key -> IO String
+generateTestCaseForKey testName key = do
+  licenseKeyB <- activate key
+
+  let Just created  = extractField licenseKeyB "Created"
+  let Just expires  = extractField licenseKeyB "Expires"
+  let Just signDate = extractField licenseKeyB "SignDate"
+
+  let licenseKey = Text.unpack . Text.decodeUtf8 $ licenseKeyB
+  let key' = key { SKM.created = created, SKM.expires = expires, SKM.signDate = signDate }
+
+  let basic = 
+        [ SKM.mandatory testName licenseKey key'
+        , SKM.features  testName licenseKey key'
+        ]
+  optional <- forM SKM.optionalFields (\field -> do
+                features1  <- fmap (SKM.withField field) $ randomRIO (1, 8191) :: IO Integer
+                features2  <- fmap (SKM.withField field) $ randomRIO (1, 8191) :: IO Integer
+                lk_with    <- fmap (Text.unpack . Text.decodeUtf8) $ activateWithFields features1 key'
+                lk_without <- fmap (Text.unpack . Text.decodeUtf8) $ activateWithFields features2 key'
+                return $ SKM.optional testName lk_with lk_without field key'
+              )
+  return . concat . List.intersperse "\n" $ (basic ++ optional)
+
+activate :: SKM.Key -> IO B.ByteString
+activate = activateWithFields 0
+
+activateWithFields :: Integer -> SKM.Key -> IO B.ByteString
+activateWithFields fields key = do
+  let url = concat
+         [ "https://serialkeymanager.com/api/key/Activate"
+         , "?token=WyI1NzQiLCJUclYzcEJLQnFvaUpsd1oyVTNFN3dVK2M4QWZNcGY0WjNxSDF4RitLIl0="
+         , "&ProductId=" ++ (show $ SKM.productId key)
+         , "&Key=" ++ SKM.key key
+         , "&Sign=true"
+         , "&SignMethod=1"
+         , "&FieldsToReturn=" ++ (show $ fields)
+         , "&MachineCode=machine1"
+         ]
+
+  r <- Wreq.get url
+  let Just licenseKey = extractLicenseKey =<< (r ^? Wreq.responseBody . Aeson.key "licenseKey")
+  return licenseKey
+
+extractLicenseKey :: Aeson.Value -> Maybe B.ByteString
+extractLicenseKey v = do
+  base64 <- case v of
+              Aeson.String s -> Just $ Text.encodeUtf8 s
+              _              -> Nothing
+  case B.decode base64 of
+    Left _  -> Nothing
+    Right s -> Just s
+
+extractField licenseKey field = do
+  f <- licenseKey ^? Aeson.key field
+  case Aeson.fromJSON f of
+    Aeson.Success x -> Just x
+    _               -> Nothing
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,8 @@ module Lib where
 
 import Prelude hiding (id)
 
+import Data.Bits
+
 data Key = KeyContents
   { productId :: Integer
   , id :: Integer
@@ -21,11 +23,11 @@ data Key = KeyContents
   , block :: Bool
   , globalId :: Integer
   , customer :: ()
-  , activatedMachines :: ()
+  , activatedMachines :: [String]
   , trialActivation :: Bool
   , maxnoofmachines :: Integer
   , allowedMachines :: [String]
-  , dataObjects :: ()
+  , dataObjects :: [String]
   , signDate :: Integer
   }
 
@@ -64,6 +66,38 @@ optionalFields = [ Id, Key, Notes, GlobalId, Customer, ActivatedMachines
                  , Maxnoofmachines, AllowedMachines, DataObjects
                  ]
 
+fieldsToReturnBit :: Field -> Int
+fieldsToReturnBit ProductId         = error "Mandatory field"
+fieldsToReturnBit Id                = 1
+fieldsToReturnBit Key               = 2
+fieldsToReturnBit Created           = error "Mandatory field"
+fieldsToReturnBit Expires           = error "Mandatory field"
+fieldsToReturnBit Period            = error "Mandatory field"
+fieldsToReturnBit F1                = error "Mandatory field"
+fieldsToReturnBit F2                = error "Mandatory field"
+fieldsToReturnBit F3                = error "Mandatory field"
+fieldsToReturnBit F4                = error "Mandatory field"
+fieldsToReturnBit F5                = error "Mandatory field"
+fieldsToReturnBit F6                = error "Mandatory field"
+fieldsToReturnBit F7                = error "Mandatory field"
+fieldsToReturnBit F8                = error "Mandatory field"
+fieldsToReturnBit Notes             = 3
+fieldsToReturnBit Block             = error "Mandatory field"
+fieldsToReturnBit GlobalId          = 4
+fieldsToReturnBit Customer          = 5
+fieldsToReturnBit ActivatedMachines = 6
+fieldsToReturnBit TrialActivation   = error "Mandatory field"
+fieldsToReturnBit Maxnoofmachines   = 8
+fieldsToReturnBit AllowedMachines   = 7
+fieldsToReturnBit DataObjects       = 9
+fieldsToReturnBit SignDate          = error "Mandatory field"
+
+withField :: Bits a => Field -> a -> a
+withField field x = x `setBit` (fieldsToReturnBit field)
+
+withoutField :: Bits a => Field -> a -> a
+withoutField field x = x `clearBit` (fieldsToReturnBit field)
+
 accessor :: Field -> String
 accessor ProductId         = "get_product_id()"
 accessor Id                = "get_id()"
@@ -90,82 +124,57 @@ accessor AllowedMachines   = "get_allowed_machines()"
 accessor DataObjects       = "get_data_objects()"
 accessor SignDate          = "get_sign_date()"
 
-k1 :: Key
-k1 = KeyContents
-  { productId = 3720
-  , id = 0
-  , key = "asdf"
-  , created = 0
-  , expires = 0
-  , period = 0
-  , f1 = False
-  , f2 = True
-  , f3 = False
-  , f4 = True
-  , f5 = False
-  , f6 = True
-  , f7 = False
-  , f8 = True
-  , notes = "asdf"
-  , block = False
-  , globalId = 0
-  , customer = ()
-  , activatedMachines = ()
-  , trialActivation = False
-  , maxnoofmachines = 100
-  , allowedMachines = []
-  , dataObjects = ()
-  , signDate = 0
-  }
-
 mandatory :: String -> String -> Key -> String
-mandatory testName license expected = 
-    "TEST(" ++ testName ++ ", Mandatory) {\n"
- ++ "  std::string license{\"" ++ license ++ "\"};\n"
- ++ "  optional<LicenseKey> license_key = LicenseKey::make_unsafe(license);\n"
- ++ "\n"
- ++ "  ASSERT_TRUE(license_key.has_value()) << \"Failed to construct LicenseKey object\";\n"
- ++ "\n"
- ++ "  EXPECT_EQ(license_key->get_product_id(), " ++ (show $ productId expected) ++ ");\n"
- ++ "  EXPECT_EQ(license_key->get_created(), " ++ (show $ created expected) ++ ");\n"
- ++ "  EXPECT_EQ(license_key->get_expires(), " ++ (show $ expires expected) ++ ");\n"
- ++ "  EXPECT_EQ(license_key->get_period(), " ++ (show $ period expected) ++ ");\n"
- ++ "  EXPECT_EQ(license_key->get_block(), " ++ (show $ block expected) ++ ");\n"
- ++ "  EXPECT_EQ(license_key->get_trial_activation(), " ++ (show $ trialActivation expected) ++ ");\n"
- ++ "  EXPECT_EQ(license_key->get_sign_date(), " ++ (show $ signDate expected) ++ ");\n"
- ++ "}\n"
+mandatory testName license expected = concat
+  [ "TEST(" ++ testName ++ ", Mandatory) {\n"
+  , "  std::string license{" ++ show license ++ "};\n"
+  , "  optional<LicenseKey> license_key = LicenseKey::make_unsafe(license);\n"
+  , "\n"
+  , "  ASSERT_TRUE(license_key.has_value()) << \"Failed to construct LicenseKey object\";\n"
+  , "\n"
+  , "  EXPECT_EQ(license_key->get_product_id(), " ++ (show $ productId expected) ++ ");\n"
+  , "  EXPECT_EQ(license_key->get_created(), " ++ (show $ created expected) ++ ");\n"
+  , "  EXPECT_EQ(license_key->get_expires(), " ++ (show $ expires expected) ++ ");\n"
+  , "  EXPECT_EQ(license_key->get_period(), " ++ (show $ period expected) ++ ");\n"
+  , "  EXPECT_EQ(license_key->get_block(), " ++ (show $ block expected) ++ ");\n"
+  , "  EXPECT_EQ(license_key->get_trial_activation(), " ++ (show $ trialActivation expected) ++ ");\n"
+  , "  EXPECT_EQ(license_key->get_sign_date(), " ++ (show $ signDate expected) ++ ");\n"
+  , "}\n"
+  ]
 
 features :: String -> String -> Key -> String
-features testName license expected = 
-    "TEST(" ++ testName ++ ", Features) {\n"
- ++ "  FeatureChecker f;\n"
- ++ "  f.license = \"" ++ license ++ "\";\n"
- ++ "  f.f1 = " ++ (show $ f1 expected) ++ ";\n"
- ++ "  f.f2 = " ++ (show $ f2 expected) ++ ";\n"
- ++ "  f.f3 = " ++ (show $ f3 expected) ++ ";\n"
- ++ "  f.f4 = " ++ (show $ f4 expected) ++ ";\n"
- ++ "  f.f5 = " ++ (show $ f5 expected) ++ ";\n"
- ++ "  f.f6 = " ++ (show $ f6 expected) ++ ";\n"
- ++ "  f.f7 = " ++ (show $ f7 expected) ++ ";\n"
- ++ "  f.f8 = " ++ (show $ f8 expected) ++ ";\n"
- ++ "\n"
- ++ "  f.check();\n"
- ++ "}\n"
+features testName license expected = concat
+  [ "TEST(" ++ testName ++ ", Features) {\n"
+  , "  FeatureChecker f;\n"
+  , "  f.license = " ++ show license ++ ";\n"
+  , "  f.f1 = " ++ (show $ f1 expected) ++ ";\n"
+  , "  f.f2 = " ++ (show $ f2 expected) ++ ";\n"
+  , "  f.f3 = " ++ (show $ f3 expected) ++ ";\n"
+  , "  f.f4 = " ++ (show $ f4 expected) ++ ";\n"
+  , "  f.f5 = " ++ (show $ f5 expected) ++ ";\n"
+  , "  f.f6 = " ++ (show $ f6 expected) ++ ";\n"
+  , "  f.f7 = " ++ (show $ f7 expected) ++ ";\n"
+  , "  f.f8 = " ++ (show $ f8 expected) ++ ";\n"
+  , "\n"
+  , "  f.check();\n"
+  , "}\n"
+  ]
 
 optional :: String -> String -> String -> Field -> Key -> String
-optional testName with_field without_field field expected =
-    "TEST(" ++ testName ++ ", " ++ show field ++ ") {\n"
- ++ "  std::string license1{\"" ++ with_field ++ "\"};\n"
- ++ "  optional<LicenseKey> license_key_1 = LicenseKey::make_unsafe(license1);\n"
- ++ "\n"
- ++ "  std::string license2{\"" ++ without_field ++ "\"};\n"
- ++ "  optional<LicenseKey> license_key_2 = LicenseKey::make_unsafe(license2);\n"
- ++ "  ASSERT_TRUE(license_key_1.has_value()) << \"Failed to construct LicenseKey object\";\n"
- ++ "  ASSERT_TRUE(license_key_2.has_value()) << \"Failed to construct LicenseKey object\";\n"
- ++ "\n"
- ++ "  EXPECT_TRUE(license_key_1->" ++ accessor field ++ ".has_value());\n"
- ++ "  EXPECT_FALSE(license_key_2->" ++ accessor field ++ ".has_value());\n"
- ++ "}\n"
+optional testName with_field without_field field expected = concat
+  [ "TEST(" ++ testName ++ ", " ++ show field ++ ") {\n"
+  , "  std::string license1{" ++ show with_field ++ "};\n"
+  , "  optional<LicenseKey> license_key_1 = LicenseKey::make_unsafe(license1);\n"
+  , "\n"
+  , "  std::string license2{" ++ show without_field ++ "};\n"
+  , "  optional<LicenseKey> license_key_2 = LicenseKey::make_unsafe(license2);\n"
+  , "  ASSERT_TRUE(license_key_1.has_value()) << \"Failed to construct LicenseKey object\";\n"
+  , "  ASSERT_TRUE(license_key_2.has_value()) << \"Failed to construct LicenseKey object\";\n"
+  , "\n"
+  , "  EXPECT_TRUE(license_key_1->" ++ accessor field ++ ".has_value());\n"
+  , "  EXPECT_FALSE(license_key_2->" ++ accessor field ++ ".has_value());\n"
+  , "}\n"
+  ]
 
 generateTest :: String -> Key -> String
 generateTest testName expected =
